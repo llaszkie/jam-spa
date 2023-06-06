7339d9b1d76ea880f88055de2b86ffb5612dbfd6
Łukasz Łaszkiewicz <lukasz.laszkiewicz@capgemini.com>
2023-06-06 15:38:57 +0200

    chore: test fix, start script

--- http-server.py
+++ http-server.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Inspired by https://gist.github.com/jtangelder/e445e9a7f5e31c220be6
 # Python3 http.server for Single Page Application
 
+import argparse
 import urllib.parse
 import http.server
 import socketserver
 import re
 from pathlib import Path
+import os 
 
 HOST = ('0.0.0.0', 8000)
 pattern = re.compile('.png|.jpg|.jpeg|.js|.css|.ico|.gif|.svg', re.IGNORECASE)
@@ -25,5 +27,16 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 
-httpd = socketserver.TCPServer(HOST, Handler)
-httpd.serve_forever()
+def run_server(folder):
+    os.chdir(folder)
+    httpd = socketserver.TCPServer(HOST, Handler)
+    print(f"Serving files from {folder} on http://{HOST[0]}:{HOST[1]}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple HTTP Server")
+    parser.add_argument("folder", help="Folder to serve", default=".")
+    args = parser.parse_args()
+
+    run_server(args.folder)
