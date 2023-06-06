57c4b266d97ef2c0f3fa56dc159e157e977cd1cd
Łukasz Łaszkiewicz <lukasz.laszkiewicz@capgemini.com>
2023-06-06 16:05:40 +0200

    chore: nx 16

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
-const { join } = require('path');
+const { createGlobPatternsForDependencies } = require("@nx/react/tailwind");
+const { join } = require("path");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     join(
       __dirname,
-      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      "{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}"
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
